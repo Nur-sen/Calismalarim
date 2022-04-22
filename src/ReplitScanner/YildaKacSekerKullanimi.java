ad856778377a40a6e9149f1b166819fe500e4093
Nur-sen <nursenalkan84@gmail.com>
2022-04-22 12:53:03 +0300

    22.02.2022

--- src/ReplitScanner/YildaKacSekerKullanimi.java
+++ src/ReplitScanner/YildaKacSekerKullanimi.java
@@ -9,9 +9,7 @@ public class YildaKacSekerKullanimi {
         Yılda kaç kg şeker kullandığını hesaplayın ve yazdırın.
 
        1 şeker = 1.7 gr
-
        Örnek Çıktı:
-
       Yılda 12.41 kg şeker kullanıyor.
 
          */
@@ -21,6 +19,7 @@ public class YildaKacSekerKullanimi {
         System.out.print("Bir bardaga kac seker atiyorsunuz: ");
         double sekerKullanimi=scan.nextDouble();
         double birSeker=1.7 ;
+
         sekerKullanimi*=birSeker;
 
         double yilliksekerMiktari= cayBardagi*sekerKullanimi*365;//

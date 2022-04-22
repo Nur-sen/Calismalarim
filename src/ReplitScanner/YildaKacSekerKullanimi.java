646e12bbae4650c5b0102847d4489ffa7473e26e
Nur-sen <nursenalkan84@gmail.com>
2022-04-22 12:04:50 +0300

    22.04.2022

---
+++ src/ReplitScanner/YildaKacSekerKullanimi.java
@@ -0,0 +1,32 @@
+package ReplitScanner;
+
+import java.util.Scanner;
+
+public class YildaKacSekerKullanimi {
+    public static void main(String[] args) {
+        /*
+        Kullanıcıya günde ne kadar çay içtiğini ve ne kadar şeker kullandığını sorun.
+        Yılda kaç kg şeker kullandığını hesaplayın ve yazdırın.
+
+       1 şeker = 1.7 gr
+
+       Örnek Çıktı:
+
+      Yılda 12.41 kg şeker kullanıyor.
+
+         */
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Gunde kac bardak cay iciyorsunuz : ");
+        int cayBardagi=scan.nextInt();
+        System.out.print("Bir bardaga kac seker atiyorsunuz: ");
+        double sekerKullanimi=scan.nextDouble();
+        double birSeker=1.7 ;
+        sekerKullanimi*=birSeker;
+
+        double yilliksekerMiktari= cayBardagi*sekerKullanimi*365;//
+
+        System.out.println("yilda " +yilliksekerMiktari + " gr seker kullaniyor");
+
+
+    }
+}

ed087e97c9d9a785f5865bcbb0405779c7185aad
Nur-sen <nursenalkan842gmail.com>
2022-04-22 13:52:36 +0300

    22.04.2022

--- src/ReplitScanner/C01_yenicalisma.java
+++ src/ReplitScanner/C01_yenicalisma.java
@@ -1,4 +1,37 @@
 package ReplitScanner;
 
+import java.util.Scanner;
+
 public class C01_yenicalisma {
+    public static void main(String[] args) {
+        /*
+        int veri türünün Maximum ve Minimum değerlerini gösteren kodu konsola yazınız.*/
+
+
+        int arr []={10, 60, 20, 3, 5, -1};
+
+        int max=arr[0];
+        int min =arr[0];
+
+
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+
+        }
+        System.out.println("en buyuk sayi = " + max);
+
+        for (int i = 0; i < arr.length ; i++) {
+
+            if(arr[i]<min){
+                min=arr[i];
+            }
+           
+        }
+        System.out.println("en kucuk sayi = " + min);
+
+
+
+    }
 }
